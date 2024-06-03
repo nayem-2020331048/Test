8fe196a0bce0666b1d1ae0ae7ae4ee2e8b983c2f
nayem-2020331048 <n45islam@gmail.com>
2024-06-03 16:28:10 +0600

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-// Import the Express module
+// Initialization
 const express = require('express');
-
-// Create an Express application
 const app = express();
 
-// Define a route that handles GET requests to the root URL ("/")
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const mongoose = require('mongoose');
+const Note = require('./src/models/Note');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongoDbPath = "mongodb+srv://nayem123:nayemI2006@cluster0.s6igsjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoDbPath).then(function() {
+    app.get("/", function(req, res) {
+        const response = { statuscode: res.statusCode, message: "API Works!" };
+        res.json(response);
+    });
+    
+    const noteRouter = require('./src/routes/Note');
+    app.use("/notes", noteRouter);
 });
 
-// Start the server and listen on port 3000
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Starting the server on a PORT
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function() {
+    console.log("Server started at PORT: " + PORT);
 });
